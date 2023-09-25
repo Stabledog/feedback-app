43b78f24e519711ff3e996a5abfb7bddf2b36738
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-24 20:33:51 -0400

    Sync (auto)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-    return <h1>Hello from the app component</h1>
+    return (
+        <div className='container'>
+            <h1>
+                Hello 2 from the app <p>component</p>
+            </h1>
+        </div >
+    );
 }
 
 export default App;
