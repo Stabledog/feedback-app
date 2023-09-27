a6e365d793ce09da2b626219de36d33affd76ff5
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-27 14:37:46 -0400

    Sync (auto)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 function App() {
     const title = "Blog post";
     const body = "This is my blog post body";
-
     const comments = [
         { id: 1, text: "Comment one" },
         { id: 2, text: "Comment two" },
         { id: 3, text: "Comment three" },
     ];
+    const comment_block = (<div className="comments">
+                    <h3>Comments {comments.length}</h3>
+                    {comments.map((comment, index) => (
+                        <li key={index}> {comment.text}</li>
+                    ))}
+                </div>);
+
 
     const loading = false;
 
@@ -19,14 +25,7 @@ function App() {
         <div className="container">
             <h1>{title}</h1>
             <p>{body + ":" + Math.random() * 4}</p>
-            {showComments && (
-                <div className="comments">
-                    <h3>Comments {comments.length}</h3>
-                    {comments.map((comment, index) => (
-                        <li key={index}> {comment.text}</li>
-                    ))}
-                </div>
-            )}
+            {showComments && comment_block}
         </div>
     );
 }
