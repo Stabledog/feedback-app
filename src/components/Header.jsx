adb5f1e3de3da2b70c55ec811fbba068011875e8
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-27 17:02:43 -0400

    Sync (auto)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-function Header(props) {
+function Header({text}) {
     return (
         <div>
-            <h2>{props.text}</h2>
+            <h2>{text}</h2>
         </div>
     )
+}
+
+Header.defaultProps = {
+    "text": 'Feedback UI',
 }
 
 export default Header
