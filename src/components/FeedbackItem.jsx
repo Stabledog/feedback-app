c7bf54b26c3863228f6052a99f3d38529e8751b0
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-27 18:55:07 -0400

    8:04

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,18 +4,10 @@ function FeedbackItem() {
     const [rating, setRating] = useState(7);
     const [text, setText] = useState("This is an item");
 
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log(`prev=${prev}`);
-            return prev<=9 && prev+1 || 10;
-        });
-    }
-
     return (
         <div className="card">
             <div className="num-display">{rating}</div>
             <div className="text-display">{text}</div>
-            <button onClick={handleClick}>Click</button>
         </div>
     );
 }
