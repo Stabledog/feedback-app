5ac27eaba4a34afe930625c711f2fa2a1e792f78
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-27 13:48:26 -0400

    @4:27 in Dynamic Values and Lists in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 function App() {
+    const title = 'Blog post';
+    const body = 'This is my blog post body';
+
+    const comments = [
+        { id:1, text: 'Comment one'},
+        { id:2, text: 'Comment two'},
+        { id:3, text: 'Comment three'},
+    ];
+
     return (
         <div className='container'>
-            <h1>
-                Hello 2 from the app <p>component</p>
-            </h1>
+            <h1>{title}</h1>
+            <p>{body + ":" + Math.random() * 4}</p>
+            <div className="comments">
+                <h3>Comments {comments.length}</h3>
+                {
+                    comments.map(
+                        (comment,index) => (
+                        <li>{index}:{comment.text}</li>
+                        )
+                    )
+                }
+            </div>
         </div >
     );
 }
