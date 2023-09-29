04e64c914454ac7490b5c7a84e4ac1f9c7524e95
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-29 09:17:51 -0400

    02:33 Events and Prop Drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,12 +2,16 @@ import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types';
 import Card from './shared/Card'
 
+function onClick_Close(v:any) {
+    console.log(992);
+    console.log(v);
+}
 
 function FeedbackItem({item,reverse}) {
     return (
         <Card reverse={reverse}>
             <div className="num-display">{item.rating}</div>
-            <button className='close'><FaTimes color='purple'/></button>
+            <button className='close' onClick={onClick_Close}><FaTimes color='purple'/></button>
             <div className="text-display">{item.text}</div>
         </Card>
     );
