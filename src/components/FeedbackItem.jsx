5d2a5f688571e1aee9985dcfab3e763491bfcd09
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-29 08:03:19 -0400

    Convert Card to tsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
-import Card from './shared/Card.jsx'
+import Card from './shared/Card'
+
 
 function FeedbackItem({item,reverse}) {
-
     return (
         <Card reverse={reverse}>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}</div>
         </Card>
     );
-}
+};
 
 export default FeedbackItem;
 
