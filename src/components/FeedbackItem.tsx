3ac067f48d027ffcd99ce353374ebe491e2ad473
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-29 09:25:12 -0400

    Typescriptify FeedbackItem

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -2,16 +2,22 @@ import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types';
 import Card from './shared/Card'
 
-function onClick_Close(v:any) {
+function onClick_Close(id:number,v:any): void  {
     console.log(992);
-    console.log(v);
+    console.log(id,v);
 }
 
-function FeedbackItem({item,reverse}) {
+interface FeedbackItemProps  {
+    item: any,
+    reverse: boolean
+};
+
+function FeedbackItem({item,reverse}: FeedbackItemProps) :JSX.Element {
+    console.log(item);
     return (
         <Card reverse={reverse}>
             <div className="num-display">{item.rating}</div>
-            <button className='close' onClick={onClick_Close}><FaTimes color='purple'/></button>
+            <button className='close' onClick={(v)=>onClick_Close(item.id,v)}><FaTimes color='purple'/></button>
             <div className="text-display">{item.text}</div>
         </Card>
     );
